36583243fa64f8baf37e3ecd0cca2ccd8e6935bb
Arya S <arya.surya021@gmail.com>
2020-03-05 18:42:50 +0700

    first commit

---
+++ Electroins/Electroins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Electroins
+//
+//  Created by IT Division on 05/03/20.
+//  Copyright © 2020 com.aryasurya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
